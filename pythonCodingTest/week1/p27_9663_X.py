30763aeef648ef10a128d9f5a2579c9e28420cd5
HyunggiPark <phg9587@naver.com>
2022-10-30 15:31:06 +0900

    p27 9663번 문제 - 항해 알고리즘 수업을 참고하여 코드 변경 후 다시 제출했으나 똑같이 시간초과가 난다.

--- pythonCodingTest/week1/p27_9663_X.py
+++ pythonCodingTest/week1/p27_9663_X.py
@@ -1,53 +1,48 @@
-import sys
-
 """
-q = list(map(int, sys.stdin.readline().split()))
-# q = [1, 2, 3, 4, 5]
+is_ok_on(nth_row) : 해당 row에 들어가 있는 값이 그 위의 row들에 대해서 공격받지 않는지 확인
+    - 같은 열에 존재하는지 확인.
+    - 대각선에 존재하는지 확인 => x 좌표 차이 와 y 좌표 차이가 두개 모두 다 동일하다면 대각선에 위치한다.
+        -- 둘중에 한개라도 해당되면 놓으면 안되는 배치 => False 반환
+        -- 둘 다 해당하지 않으면 놓아도 되는 배치 => True 반환
 
-n = input()  #2
-q = [sys.stdin.readline() for i in range(n)]  
-# q = ["1 2 3", "4 5 6"]
-
-n = input()  #2
-q = [list(map(int, sys.stdin.readline().split())) for i in range(n)]
-# q = [[472], [385]]
+n_queen(nth_row) : 재귀를 타면서 총 n개의 queen이 배치 되었는지 확인
+    - nth_row가 필요한 queen 수만큼이 되면 queen 을 모두 배치한 것이므로 재귀를 종료
+    - 종료조건이 되지 않는다면, queen 배치를 바꿔가면서 is_ok_on 메서드를 호출하여 True가 Return 되면 재귀를 이어나간다. 
 """
 
-quene_num = int(input())
 
-answer = 0
+def is_ok_on(nth_row):
+    nth_col = put_queen_each_row[nth_row]
+
+    for i in range(nth_row):
+        pre_row = i
+        pre_col = put_queen_each_row[pre_row]
+
+        if (nth_col == pre_col) or (nth_row - pre_row == abs(nth_col - pre_col)):
+            return False
+
+    return True
 
 
-def n_queen(list, depth):
-    global answer
-    if len(list) == quene_num:
-        answer += 1
+def n_queen(nth_row):
+    global answer_count
+
+    if nth_row >= needed_quene_num:
+        answer_count += 1
         return
 
-    temp_depth = depth
-    check_list = [True] * (quene_num + 1)
+    for col in range(needed_quene_num):
+        put_queen_each_row[nth_row] = col
 
-    for i in list:
-        my_pos = i
-        check_list[my_pos] = False
-
-        left = i - temp_depth
-        right = i + temp_depth
-        temp_depth -= 1
-
-        if 1 <= left <= quene_num:
-            check_list[left] = False
-
-        if 1 <= right <= quene_num:
-            check_list[right] = False
-
-    for i in range(1, quene_num + 1):
-        if check_list[i]:
-            list.append(i)
-            n_queen(list, depth + 1)
-            list.pop()
+        if is_ok_on(nth_row):
+            n_queen(nth_row + 1)
 
 
-n_queen(list(), 0)
+needed_quene_num = int(input())
 
-print(answer)
+put_queen_each_row = [-1] * needed_quene_num
+answer_count = 0
+
+n_queen(0)
+
+print(answer_count)
