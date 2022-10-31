b5649ce4331af81ecd3f06e2a4cb16a9df8861ad
HyunggiPark <phg9587@naver.com>
2022-11-01 01:11:34 +0900

    퀵 정렬 구현하기 : 첫 실행 - 실패

---
+++ pythonCodingTest/week1/sort/quick.py
@@ -0,0 +1,34 @@
+quiz_list = [10, 8, 7, 4, 3, 9, 6, 5, 2, 1]
+
+answer_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+list.append
+
+
+def quick_sort(list, start_cursor, end_cursor):
+    pl = start_cursor
+    pr = end_cursor
+    pivot_idx = (start_cursor + end_cursor) // 2
+
+    while pl <= pr:
+        while list[pl] < list[pivot_idx]:
+            pl += 1
+        while list[pr] > list[pivot_idx]:
+            pr -= 1
+
+            list[pl], list[pr] = list[pr], list[pl]
+            pl += 1
+            pr -= 1
+
+    quick_sort(list, start_cursor, pr)
+    quick_sort(list, pl, end_cursor)
+
+
+print("Befor Sorting : ", quiz_list)
+
+quick_sort(quiz_list, 0, len(quiz_list) - 1)
+
+print("After Sorting : ", quiz_list)
+
+print(answer_list)
+print("Sort Result :", quiz_list == answer_list)
