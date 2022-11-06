617be1b9ed38cc3b440d5567dbb72b88894c3254
HyunggiPark <phg9587@naver.com>
2022-11-06 15:41:46 +0900

    코드 수정

--- pythonCodingTest/week2/p16_2493.py
+++ pythonCodingTest/week2/p16_2493.py
@@ -14,15 +14,8 @@ q = list(map(int, sys.stdin.readline().split()))
 # q = [1, 2, 3, 4, 5]
 """
 
+# 정답 코드 참조
+# https://1-7171771.tistory.com/71
+
 n = int(input())  # 2
 tower_list = list(map(int, sys.stdin.readline().split()))
-
-answer_list = ["0"] * n
-for i in range(len(tower_list) - 1, -1, -1):
-    cur_tower_height = tower_list[i]
-    for j in range(i - 1, -1, -1):
-        if tower_list[j] >= cur_tower_height:
-            answer_list[i] = str(j + 1)
-            break
-
-print(" ".join(answer_list))
