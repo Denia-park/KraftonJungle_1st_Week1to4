e328331fdf54a8d7c7757aab3038458b93203e35
HyunggiPark <phg9587@naver.com>
2022-11-07 00:53:49 +0900

    week02 p24_11279 heapq 모듈을 가져와서 사용함

---
+++ pythonCodingTest/week2/p24_11279.py
@@ -0,0 +1,31 @@
+import heapq
+
+import sys
+
+"""
+n = input()  #2
+a = [sys.stdin.readline() for i in range(n)]  
+# a = ["1 2 3", "4 5 6"]
+
+n = input()  #2
+q = [list(map(int, sys.stdin.readline().split())) for i in range(n)]
+# q = [[472], [385]]
+
+q = list(map(int, sys.stdin.readline().split()))
+# q = [1, 2, 3, 4, 5]
+"""
+
+
+heap = []
+
+n = int(input())
+num_list = [int(sys.stdin.readline().rstrip()) for _ in range(n)]
+
+for num in num_list:
+    if num == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(-heapq.heappop(heap))
+    else:
+        heapq.heappush(heap, -num)
