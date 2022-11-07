173ec8ec67a468f24ef4a64addbd3c9b84a53caf
HyunggiPark <phg9587@naver.com>
2022-11-07 15:12:48 +0900

    week02 p8_1629  - 정답 참고 , 재귀 함수를 여러번 타지말고 한번 계산한 것을 계속 쓰면서 시간을 줄여야함

--- pythonCodingTest/week2/p8_1629.py
+++ pythonCodingTest/week2/p8_1629.py
@@ -13,25 +13,25 @@ q = list(map(int, sys.stdin.readline().split()))
 # q = [1, 2, 3, 4, 5]
 """
 
+# 정답 참조
+# https://st-lab.tistory.com/237
+
 A, B, C = list(map(int, sys.stdin.readline().split()))
 
 
 def divide_pow(base_num, pow_num, mod_num):
-    if pow_num == 1:
+    if pow_num == 0:
+        return 1
+    elif pow_num == 1:
         return base_num % mod_num
 
     if pow_num % 2 == 0:
-        return (
-            divide_pow(base_num, pow_num // 2, mod_num)
-            * divide_pow(base_num, pow_num // 2, mod_num)
-        ) % mod_num
+        temp_val = divide_pow(base_num, pow_num // 2, mod_num)
+        return (temp_val * temp_val) % mod_num
     else:
         pow_num = pow_num - 1
-        return (
-            divide_pow(base_num, 1, mod_num)
-            * divide_pow(base_num, pow_num // 2, mod_num)
-            * divide_pow(base_num, pow_num // 2, mod_num)
-        ) % mod_num
+        temp_val = divide_pow(base_num, pow_num // 2, mod_num)
+        return ((base_num % mod_num) * temp_val * temp_val) % mod_num
 
 
 print(divide_pow(A, B, C))
