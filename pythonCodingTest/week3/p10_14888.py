a3d920e90c8d5401c56f4d9f4e2ac4d36212af4f
HyunggiPark <phg9587@naver.com>
2022-11-14 14:32:18 +0900

    week03 p10_14888 정답 코드 참고 , dfs로 계산이 필요할 떄는 계산된 값을 매개변수로 넘기는게 빠르다.

--- pythonCodingTest/week3/p10_14888.py
+++ pythonCodingTest/week3/p10_14888.py
@@ -17,73 +17,54 @@ q = list(map(int, sys.stdin.readline().split()))
 # q = [1, 2, 3, 4, 5]
 """
 
+# 정답 코드 참조
+# https://st-lab.tistory.com/121
+
+# 처음에 직접 dfs로 풀기는 했으나 계속해서 시간초과가 나서
+# 정답을 확인함.
+
+# 계산 과정을 따로 함수로 처리했더니 시간이 오래걸린 것 같다.
+    # 해결 방법 (둘 다 적용하거나 한개만 적용하거나 자유롭게)
+        # 1. dfs를 for문 대신에 4개로 분리하거나 
+        # 2. 매개변수로 넘길때 계산을 하고 넘기는게 훨씬 빠르다.
+
+
+def dfs_2(cur_val, deepth):
+    global max_val, min_val
+
+    if deepth == (NUMBER_NUM - 1):
+        max_val = max(max_val, cur_val)
+        min_val = min(min_val, cur_val)
+        return
+
+    for idx in range(4):
+        if operartors_counts[idx] == 0:
+            continue
+
+        operartors_counts[idx] -= 1
+
+        if idx == 0:  # +
+            dfs_2(cur_val + numbers[deepth + 1], deepth + 1)
+        elif idx == 1:  # -
+            dfs_2(cur_val - numbers[deepth + 1], deepth + 1)
+        elif idx == 2:  # *
+            dfs_2(cur_val * numbers[deepth + 1], deepth + 1)
+        elif idx == 3:  # //
+            if cur_val / numbers[deepth + 1] < 0:
+                dfs_2(math.ceil(cur_val / numbers[deepth + 1]), deepth + 1)
+            else:
+                dfs_2(math.floor(cur_val / numbers[deepth + 1]), deepth + 1)
+
+        operartors_counts[idx] += 1
+
+
 NUMBER_NUM = int(input())
 numbers = list(map(int, sys.stdin.readline().split()))
 operartors_counts = list(map(int, sys.stdin.readline().split()))
+max_val = -(10**9)
+min_val = 10**9
 
-my_numbers_deque = deque()
-for value in numbers:
-    my_numbers_deque.append(value)
+dfs_2(numbers[0], 0)
 
-default_op = ["+", "-", "*", "//"]
-operators = []
-visited = [False] * (NUMBER_NUM - 1)
-
-for idx, count in enumerate(operartors_counts):
-    op = default_op[idx]
-    for _ in range(count):
-        operators.append(op)
-
-# plus_num, minus_num, multiply_num, divide_num
-def calculate_q(original_q):
-    global answer, my_numbers_deque
-
-    my_q = original_q.copy()
-    temp_numbers_deque = my_numbers_deque.copy()
-
-    while len(temp_numbers_deque) != 1:
-        value1 = temp_numbers_deque.popleft()
-        value2 = temp_numbers_deque.popleft()
-        operator = my_q.popleft()
-
-        if operator == "+":
-            temp_value = value1 + value2
-        elif operator == "-":
-            temp_value = value1 - value2
-        elif operator == "*":
-            temp_value = value1 * value2
-        elif operator == "//":
-            if (value1 / value2) <= 0:
-                temp_value = math.ceil(value1 / value2)
-            else:
-                temp_value = math.floor(value1 / value2)
-
-        temp_numbers_deque.appendleft(temp_value)
-
-    return temp_numbers_deque.popleft()
-
-
-def dfs(my_q):
-    global answer
-
-    if len(my_q) == (NUMBER_NUM - 1):
-        answer.append(calculate_q(my_q))
-        return
-
-    for cur_op_idx in range(len(operators)):
-        if not visited[cur_op_idx]:
-            visited[cur_op_idx] = True
-            my_q.append(operators[cur_op_idx])
-            dfs(my_q)
-            visited[cur_op_idx] = False
-            my_q.pop()
-
-
-my_q = deque()
-answer = []
-dfs(my_q)
-
-answer.sort()
-
-print(answer[-1])
-print(answer[0])
+print(max_val)
+print(min_val)
