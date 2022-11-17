8e97927a3c7a1ff4855f30b4a789aef1302ffae4
HyunggiPark <phg9587@naver.com>
2022-11-17 14:27:18 +0900

    week03 테스트 : p1_2078 정답 참고

--- pythonCodingTest/week3/test/p1_2078.py
+++ pythonCodingTest/week3/test/p1_2078.py
@@ -1,34 +1,27 @@
+# 정답 코드 참조
+# https://fre2-dom.tistory.com/239
+
 import sys
 
-sys.setrecursionlimit(10**6)
+a, b = map(int, sys.stdin.readline().split())
+l = 0 # 왼쪽 이동 횟수
+r = 0 # 오른쪽 이동 횟수
 
-"""
-n = input()  #2
-a = [sys.stdin.readline() for _ in range(n)]  
-# a = ["1 2 3", "4 5 6"]
+# a와 b가 1보다 클 때까지 반복한다.
+while a > 1 and b > 1:
+    # a가 더 크면 왼쪽으로 이동한 경우로
+    # a를 b로 나눈 몫은 왼쪽 이동 횟수와 더하고 나머지는 a로 초기화한다.
+    if a > b:
+        l += a // b
+        a %= b
 
-n = input()  #2
-q = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-# q = [[472], [385]]
+    # 반대로 b가 더 크면 오른쪽으로 이동한 경우로
+    # b를 a로 나눈 몫은 오른쪽 이동 횟수와 더하고 나머지는 b로 초기화한다.
+    else:
+        r += b // a
+        b %= a
 
-q = list(map(int, sys.stdin.readline().split()))
-# q = [1, 2, 3, 4, 5]
-"""
-
-l_val, r_val = list(map(int, sys.stdin.readline().split()))
-
-l_c = 0
-r_c = 0
-
-while True:
-    if l_val == 1 and r_val == 1:
-        break
-
-    if r_val > l_val:
-        r_val = r_val - l_val
-        r_c += 1
-    elif r_val < l_val:
-        l_val = l_val - r_val
-        l_c += 1
-
-print(l_c, r_c)
+# a나 b가 1보다 작아지면 남은 이동 거리를 1씩 증가시킨다.
+l += a - 1
+r += b - 1
+print(l, r)
