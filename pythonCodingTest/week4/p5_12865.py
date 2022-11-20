59e093f60883e92a0a5001e97550376528132415
HyunggiPark <phg9587@naver.com>
2022-11-21 02:34:11 +0900

    week04 p5_12865 2차원 dp를 사용하여 문제 풀이 -> 몇가지 요소를 수정해야함
    1. 2중 for문을 돌리더라도 처리하는 요소 수를 줄여서 계산량을 줄여야함
    2. 중복이 계산되지 않도록 신경 써야함.
    3. 잘 모르겠으면 반례 확인해볼 것

--- pythonCodingTest/week4/p5_12865.py
+++ pythonCodingTest/week4/p5_12865.py
@@ -14,7 +14,7 @@ q = list(map(int, sys.stdin.readline().split()))
 """
 
 # 정답 참고
-# https://www.acmicpc.net/problem/12865
+# https://st-lab.tistory.com/141
 
 WEIGHT = 0
 VALUE = 1
@@ -23,17 +23,40 @@ BAGAGE_NUM, WEIGHT_LIMIT = map(int, sys.stdin.readline().split())
 
 BAGAGE_LIST = [list(map(int, sys.stdin.readline().split())) for _ in range(BAGAGE_NUM)]
 
-dp = [[0] * (WEIGHT_LIMIT + 1) for _ in range(BAGAGE_NUM + 1)]
+# BAGAGE_LIST.sort()
 
-for cur_weight in range(1, WEIGHT_LIMIT + 1):
-    for bagage_idx in range(1, BAGAGE_NUM + 1):
-        if cur_weight < BAGAGE_LIST[bagage_idx - 1][WEIGHT]:
-            dp[bagage_idx][cur_weight] = dp[bagage_idx - 1][cur_weight]
-        else:
-            dp[bagage_idx][cur_weight] = max(
-                dp[bagage_idx - 1][cur_weight],
-                dp[bagage_idx - 1][cur_weight - BAGAGE_LIST[bagage_idx - 1][WEIGHT]]
-                + BAGAGE_LIST[bagage_idx - 1][VALUE],
-            )
+dp1 = [0] * (WEIGHT_LIMIT + 1)
+dp2 = [0] * (WEIGHT_LIMIT + 1)
 
-print(dp[BAGAGE_NUM][WEIGHT_LIMIT])
+# 내꺼 => 중복이 생김 (내가 먼저 계산을 했던 값이 한번 더 들어감)
+for bagage_idx in range(1, BAGAGE_NUM + 1):
+    for cur_weight in range(BAGAGE_LIST[bagage_idx - 1][WEIGHT], WEIGHT_LIMIT + 1):
+        dp1[cur_weight] = max(
+            dp1[cur_weight],
+            dp1[cur_weight - BAGAGE_LIST[bagage_idx - 1][WEIGHT]]
+            + BAGAGE_LIST[bagage_idx - 1][VALUE],
+        )
+
+print(dp1[WEIGHT_LIMIT])
+
+# 정답 => 중복이 생길수가 없음 (큰 값부터 처리하기 때문에)
+for bagage_idx in range(1, BAGAGE_NUM + 1):
+    for cur_weight in range(WEIGHT_LIMIT, BAGAGE_LIST[bagage_idx - 1][WEIGHT] - 1, -1):
+        dp2[cur_weight] = max(
+            dp2[cur_weight],
+            dp2[cur_weight - BAGAGE_LIST[bagage_idx - 1][WEIGHT]]
+            + BAGAGE_LIST[bagage_idx - 1][VALUE],
+        )
+
+print(dp2[WEIGHT_LIMIT])
+
+"""
+확인한 반례
+3 6
+3 14
+2 8
+3 9
+ 
+정답: 23
+출력: 22
+"""
