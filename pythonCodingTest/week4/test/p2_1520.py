33093982657db0395ccc4ed49ef897cc0b88c535
HyunggiPark <phg9587@naver.com>
2022-11-24 16:19:05 +0900

    week04 테스트 : p2_1520 DP + DFS 사용해서 풀이 , 정답을 보고 푼 사람에게 도움을 받음

--- pythonCodingTest/week4/test/p2_1520.py
+++ pythonCodingTest/week4/test/p2_1520.py
@@ -1,6 +1,6 @@
-from collections import deque
 import sys
 
+sys.setrecursionlimit(10**9)
 """
 n = input()  #2
 a = [sys.stdin.readline() for _ in range(n)]  
@@ -14,8 +14,6 @@ q = list(map(int, sys.stdin.readline().split()))
 # q = [1, 2, 3, 4, 5]
 """
 
-# bfs로 시간초과 나서 못 풀었습니다 !!
-
 
 def is_out_of_table(row, col):
     return row < 0 or row >= ROW or col < 0 or col >= COL
@@ -24,34 +22,37 @@ def is_out_of_table(row, col):
 ROW, COL = map(int, sys.stdin.readline().split())
 TABLE = [list(map(int, sys.stdin.readline().split())) for _ in range(ROW)]
 
-my_que = deque()
-
-my_que.append((0, 0))
+dp = [[-1] * COL for _ in range(ROW)]
 
 directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-count = 0
 
-while my_que:
-    c_r, c_c = my_que.popleft()
+def dfs(row, col):
 
-    if c_r == ROW - 1 and c_c == COL - 1:
-        count += 1
-        continue
+    if row == ROW - 1 and col == COL - 1:
+        return 1
 
-    # 오른쪽 , 아래쪽
-    for move_direc in range(len(directions)):
-        e_r = c_r + directions[move_direc][0]
-        e_c = c_c + directions[move_direc][1]
+    if dp[row][col] != -1:
+        return dp[row][col]
 
-        if is_out_of_table(e_r, e_c):
+    dp[row][col] = 0
+
+    for direc in directions:
+        edit_row = row + direc[0]
+        edit_col = col + direc[1]
+
+        if is_out_of_table(edit_row, edit_col):
             continue
 
-        cur_height = TABLE[c_r][c_c]
-        next_height = TABLE[e_r][e_c]
+        if TABLE[row][col] > TABLE[edit_row][edit_col]:
+            if dp[edit_row][edit_col] == -1:
+                dp[row][col] += dfs(edit_row, edit_col)
+            else:
+                dp[row][col] += dp[edit_row][edit_col]
 
-        if cur_height > next_height:
-            my_que.append((e_r, e_c))
+    return dp[row][col]
 
 
-print(count)
+dfs(0, 0)
+
+print(dp[0][0])
