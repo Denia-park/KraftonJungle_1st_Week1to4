2ac81d14d791880c48500db2087ce2e30ebc429b
HyunggiPark <phg9587@naver.com>
2022-11-24 18:18:53 +0900

    week04 테스트 : p3_1379 정답 참고 - 우선순위 큐 사용 해야 하며 미리 정렬을 해야함

--- pythonCodingTest/week4/test/p3_1379.py
+++ pythonCodingTest/week4/test/p3_1379.py
@@ -1,59 +1,39 @@
-import heapq
-import sys
+from heapq import heappush, heappop
 
-"""
-n = input()  #2
-a = [sys.stdin.readline() for _ in range(n)]  
-# a = ["1 2 3", "4 5 6"]
+# 정답 참고
+# https://yunny-p.tistory.com/m/entry/1379
 
-n = input()  #2
-q = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-# q = [[472], [385]]
+n = int(input())
+end_time_priority_que = []
+lect_num_room = [0] * (n + 1)
+lect_info_list = []
 
-q = list(map(int, sys.stdin.readline().split()))
-# q = [1, 2, 3, 4, 5]
-"""
+for _ in range(n):
+    lect_num, start, end = map(int, input().split())
+    lect_info_list.append([start, end, lect_num])
 
-LECTURE_NUM = int(input())
+lect_info_list = sorted(lect_info_list, key=lambda x: (x[0]))
 
-LECTURE_LIST = [
-    list(map(int, sys.stdin.readline().split())) for _ in range(LECTURE_NUM)
-]
+room_num = 0
 
-priority_que = []
+for cur_start, cur_end, cur_lect_num in lect_info_list:
+    if end_time_priority_que:
+        first_end_lecture = end_time_priority_que[0]
+        first_end_time = first_end_lecture[0]
+        if first_end_time <= cur_start:
+            occupied_lect_num = first_end_lecture[2]
 
-answer_idx_list = []
-
-lect_infos = [None] * (LECTURE_NUM + 1)
-room_infos = [None] * (LECTURE_NUM + 1)
-
-for lect in LECTURE_LIST:
-    lect_num, start, end = lect
-
-    heapq.heappush(priority_que, (start, lect_num, end))
-
-    answer_idx_list.append(lect_num)
-
-sorted_lecture_list = sorted(LECTURE_LIST, key=lambda x: x[1])
-
-max_room_idx = 0
-
-while priority_que:
-    start, lect_num, end = heapq.heappop(priority_que)
-
-    for room_idx in range(1, len(room_infos)):
-        if room_infos[room_idx] is None:
-            room_infos[room_idx] = (lect_num, end)
-            lect_infos[lect_num] = room_idx
-            max_room_idx = max(max_room_idx, room_idx)
-            break
+            lect_num_room[cur_lect_num] = lect_num_room[occupied_lect_num]
+            heappop(end_time_priority_que)
         else:
-            if room_infos[room_idx][1] <= start:
-                room_infos[room_idx] = (lect_num, end)
-                lect_infos[lect_num] = room_idx
-                max_room_idx = max(max_room_idx, room_idx)
-                break
+            room_num += 1
+            lect_num_room[cur_lect_num] = room_num
+    else:
+        room_num += 1
+        lect_num_room[cur_lect_num] = room_num
 
-print(max_room_idx)
-for idx in answer_idx_list:
-    print(lect_infos[idx])
+    heappush(end_time_priority_que, [cur_end, cur_start, cur_lect_num])
+
+print(room_num)
+for i in lect_num_room[1:]:
+    print(i)
